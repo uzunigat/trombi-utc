962ac76440ccd9da60acd8005d28bbf105f5e772
Rodrigo Uriel Zuniga Tellez <rodrigo-uriel.zuniga-tellez@etu.utc.fr>
2021-05-26 17:17:02 +0200

    Feat: Router Pages

--- src/components/CHeader.js
+++ src/components/CHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Navbar} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 // Import Components
 import logo from "../utc.svg";
@@ -10,16 +11,19 @@ const CHeader = () => {
     return (
 
         <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#">
-          <img
-            alt="logo"
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          Trombi GI
-        </Navbar.Brand>
+        
+        <Link to="/">
+          <Navbar.Brand href="#">
+            <img
+              alt="logo"
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Trombi GI
+          </Navbar.Brand>
+        </Link>
       </Navbar>
 
     );
--- src/components/formComponents/CForm.js
+++ src/components/formComponents/CForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Form, Button} from "react-bootstrap";
 
+
 // Import Components
 import FormElement from "./CFormElement";
 import FormSelect from "./CFormSelect";
@@ -11,11 +12,30 @@ const CForm = () => {
 
   const roleElements = ["Enseignant", "Chercheur", "Doctorat", "Etudiant"];
   const places = ["All", "Laboratoire Heudiasyc", "Laboratoire LMAC"];
-    
+  
+    const search = () => {
+
+      let url = 'https://webservices.utc.fr/api/v1/trombi/gi';
+
+      let username = 'wsuser';
+      let password = 'v3Kenobi!';
+
+      let headers = new Headers();
+
+      headers.set('Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64'));
+
+      fetch(url, {method:'GET',
+              headers: headers,
+
+            })
+      .then(response => response.json())
+      .then(json => console.log(json));
+
+    };
+
     return (
 
       <Form>
-
             <FormElement labelName="Nom" />
 
             <FormElement labelName="Prenom" />
@@ -24,7 +44,11 @@ const CForm = () => {
 
             <FormRadio places={places} />
 
-            <Button variant="success" className="d-flex justify-content-center mt-3" >Search</Button>
+            <div className="text-center mt-2"> 
+
+               <Button onClick={search} variant="outline-success" size="lg" >Search</Button>
+            
+            </div>
 
       </Form>
 
