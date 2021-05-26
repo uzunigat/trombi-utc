d11c1708ef51172f6d60f1d85e289261f203e7c7
Rodrigo Uriel Zuniga Tellez <rodrigo-uriel.zuniga-tellez@etu.utc.fr>
2021-05-26 18:27:56 +0200

    Feat: Loading icon

--- src/components/formComponents/CForm.js
+++ src/components/formComponents/CForm.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Form, Button} from "react-bootstrap";
-
+import {Form, Button, Spinner} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
 
 // Import Components
 import FormElement from "./CFormElement";
@@ -10,11 +10,14 @@ import FormRadio from "./CFormRadio";
  
 const CForm = () => {
 
+  const history = useHistory();
   const roleElements = ["Enseignant", "Chercheur", "Doctorat", "Etudiant"];
   const places = ["All", "Laboratoire Heudiasyc", "Laboratoire LMAC"];
+  const [loading, setLoading] = useState(false);
   
     const search = () => {
 
+      setLoading(true);
       let url = 'https://webservices.utc.fr/api/v1/trombi/gi';
 
       let username = 'wsuser';
@@ -28,8 +31,18 @@ const CForm = () => {
               headers: headers,
 
             })
-      .then(response => response.json())
-      .then(json => console.log(json));
+      .then(response => {
+        
+
+        response.json();
+      
+      })
+      .then(json => {
+        
+        history.push("/ListPage");
+        console.log(json);
+      
+      });
 
     };
 
@@ -42,13 +55,18 @@ const CForm = () => {
 
             <FormSelect elements={roleElements} />
 
+            <br /> <br />
+
             <FormRadio places={places} />
 
             <div className="text-center mt-2"> 
 
-               <Button onClick={search} variant="outline-success" size="lg" >Search</Button>
+               <Button onClick={search} variant="outline-success" size="lg" >Search</Button> <br /> <br />
+                {!loading ? true : <Spinner animation="border" variant="warning" />}
             
             </div>
+
+            
 
       </Form>
 
