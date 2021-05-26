7948c8e09a22f7def39dec331d84a603cee5ff9a
Julio Cesar Camargo <deyssoret@gmail.com>
2021-05-26 11:43:53 +0200

    pagina de inicio mas o menos hecha , falta centrar y posiblemente acomodar el codigo ( pasarlo a funciones)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Button, Form, Navbar } from "react-bootstrap";
+import logo from "./utc.svg";
 function App() {
+  const typeElement = "radio";
+  const places = ["All", "Laboratoire Heudiasyc", "Laboratoire LMAC"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#">
+          <img
+            alt="logo"
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Trombi GI
+        </Navbar.Brand>
+      </Navbar>
+
+      <Container>
+        <Form>
+          <Form.Group controlId="nom">
+            <Form.Label>Nom </Form.Label>
+            <Form.Control type="text" placeholder="Nom" />
+          </Form.Group>
+          <Form.Group controlId="prenom">
+            <Form.Label>Prenom </Form.Label>
+            <Form.Control type="text" placeholder="Prenom" />
+          </Form.Group>
+          <Form.Group controlId="role">
+            <Form.Label>Role</Form.Label>
+            <Form.Control as="select">
+              <option> Enseignant </option>
+              <option>Chercheur</option>
+              <option>Doctorant </option>
+              <option>Etudiant</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label> Laboratoire </Form.Label>
+            <>
+              {places.map((place) => (
+                <Form.Check
+                  inline
+                  label={place}
+                  name="group1"
+                  type={typeElement}
+                />
+              ))}
+            </>
+          </Form.Group>
+
+          <Button variant="success">Search</Button>
+        </Form>
+      </Container>
     </div>
   );
 }
