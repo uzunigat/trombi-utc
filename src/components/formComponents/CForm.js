1d15cdc95d745ce744467aaf83c46e31c4d79528
Rodrigo Uriel Zuniga Tellez <rodrigo-uriel.zuniga-tellez@etu.utc.fr>
2021-05-27 11:23:27 +0200

    Feat: Show Personal name into ListPage with Loading animation

--- src/components/formComponents/CForm.js
+++ src/components/formComponents/CForm.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Form, Button, Spinner} from "react-bootstrap";
-import {useHistory} from "react-router-dom";
+import {Form, Button} from "react-bootstrap";
+import { useHistory } from 'react-router-dom'
+
 
 // Import Components
 import FormElement from "./CFormElement";
@@ -13,37 +13,12 @@ const CForm = () => {
   const history = useHistory();
   const roleElements = ["Enseignant", "Chercheur", "Doctorat", "Etudiant"];
   const places = ["All", "Laboratoire Heudiasyc", "Laboratoire LMAC"];
-  const [loading, setLoading] = useState(false);
+
   
     const search = () => {
 
-      setLoading(true);
-      let url = 'https://webservices.utc.fr/api/v1/trombi/gi';
-
-      let username = 'wsuser';
-      let password = 'v3Kenobi!';
-
-      let headers = new Headers();
-
-      headers.set('Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64'));
-
-      fetch(url, {method:'GET',
-              headers: headers,
-
-            })
-      .then(response => {
-        
-
-        response.json();
-      
-      })
-      .then(json => {
-        
-        history.push("/ListPage");
-        console.log(json);
-      
-      });
-
+      history.push(`/ListPage`);
+   
     };
 
     return (
@@ -62,7 +37,6 @@ const CForm = () => {
             <div className="text-center mt-2"> 
 
                <Button onClick={search} variant="outline-success" size="lg" >Search</Button> <br /> <br />
-                {!loading ? true : <Spinner animation="border" variant="warning" />}
             
             </div>
 
